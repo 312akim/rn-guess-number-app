d8dc86fed8fe5268340192d610ed9644e20c5640
Alex Kim <312akim@gmail.com>
2021-08-04 16:05:42 -0700

    Created GameScreen, userNumber state and handlers in App.js

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -43,7 +43,7 @@ const StartGameScreen = props => {
                 <NumberContainer>
                     {selectedNumber}
                 </NumberContainer>
-                <Button title="START GAME"/>
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} />
             </Card>
         )
     }
