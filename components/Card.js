8e612ebb84603841dabab21d000e980bed7bb31e
Alex Kim <312akim@gmail.com>
2021-08-03 17:55:29 -0700

    Setup separate wrapper Card for styling

---
+++ components/Card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Card = (props) => {
+    return (
+       <View style={{...styles.card, ...props.style}}>
+           {props.children}
+       </View>     
+    );
+}
+
+styles = StyleSheet.create({
+    card: {
+        padding: 20,
+        borderRadius: 10,
+        //iOS shadow ***
+        shadowColor: 'rgba(0,0,0,0)',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        //iOS shadow end ***
+        //Android shadow ***
+        elevation: 12,
+        //Android shadow end ***
+        backgroundColor: 'white',
+    },
+});
+
+export default Card;
