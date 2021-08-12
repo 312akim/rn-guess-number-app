764910c16d251fcff95734f48232a200a058ea89
Alex Kim <312akim@gmail.com>
2021-08-12 14:00:32 -0700

    Responsive Update: GameScreen Dimensions API styling

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, Alert, ScrollView, Dimensions } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import Card from '../components/Card';
 import GameButton from '../components/GameButton';
@@ -89,13 +89,13 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 400,
         maxWidth: '90%',
     },
     listContainer: {
         flex: 1,
-        width: '80%',
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%',
     },
     list: {
       flexGrow: 1,
@@ -110,7 +110,7 @@ const styles = StyleSheet.create({
         padding: 15,
         marginVertical: 10,
         backgroundColor: 'white',
-        width: '60%',
+        width: '100%',
     }
 })
 
