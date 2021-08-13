c94a8f1c2f15e47ecec02376fedaed4d0715f75a
Alex Kim <312akim@gmail.com>
2021-08-13 15:23:05 -0700

    Update: GameOverScreen verticalPadding added

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -30,6 +30,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        paddingVertical: 15,
     },
     image: {
         height: '100%',
